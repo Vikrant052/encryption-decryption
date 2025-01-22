d5992ba79029482c60dc50bdacaaff0040c7307e
vikrant <vickrantgoswmai123@gmail.com>
2025-01-22 16:08:29 +0530

    remove host

--- enviornment/enc_dec/enc_dec/settings.py
+++ enviornment/enc_dec/enc_dec/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-imjuzjau$s6+knj$)_#m_nro7-u)qd=1y)ptske5k=ue&-0(uh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
